9238856b0199b9d18de7c3a3673b8fb50b7b9955
hirotask <hirotaskhashi@gmail.com>
2024-06-07 23:35:11 +0900

    feat: fish config

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+# Set aliases
+
+alias ls='lsd'
+alias ll='lsd -abghHliS'
+alias cat='bat'
+alias rmdir="rm -rf"
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -i'
+alias cd.='cd ./'
+alias cd..='cd ../'
+alias cd...='cd ../../'
+alias reload='exec $SHELL -l'
