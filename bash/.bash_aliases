8c8b3ad3d5e7cfb0130d1ce91c84bfefe8f33188
hirotask <hiroto.hashi@systemi.co.jp>
2024-06-08 19:07:42 +0900

    chore: wsl用の設定を若干変更

--- bash/.bash_aliases
+++ bash/.bash_aliases
@@ -1,7 +1,7 @@
 # Alias definitions
 
-alias ls='exa'
-alias ll='exa -abghHliS'
+alias ls='lsd'
+alias ll='lsd -abghHliS'
 alias cat='bat'
 alias rmdir="rm -rf"
 alias cp='cp -i'
--- fish/config_wsl.fish
+++ fish/config_wsl.fish
@@ -1,12 +1,10 @@
 # Set aliases
-
 alias gh='hub browse $(ghq list | peco | cut -d "/" -f 2,3)'
 alias vim='nvim'
 alias g='cd $(ghq root)/$(ghq list | peco)'
 alias ls='exa'
 alias ll='ls -abghHliS'
 alias la='ls -a'
-alias lt='ls --tree'
 alias cat='bat'
 alias rmdir="rm -rf"
 alias cp='cp -i'
@@ -21,7 +19,7 @@ alias reload='exec $SHELL -l'
 set -g theme_display_date yes
 set -g theme_date_format "+%F %H:%M"
 
-# display branch nameß
+# display branch name
 set -g theme_display_git_master_branch yes
 
 # Customize title bar
@@ -32,3 +30,8 @@ set -g theme_title_display_path no
 # Setup ssh-agent
 eval (ssh-agent -c)
 ssh-add /home/hiroto/.ssh/id_ed25519
+
+# Adding PATH
+set -gx RUSTUP_HOME $HOME/.rustup
+set -gx CARGO_HOME $HOME/.cargo
+fish_add_path $CARGO_HOME/bin
