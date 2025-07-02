dd36952ad6b8d4289b26c117643e74cbaa13b6df
hirotask <hiroto.hashi@systemi.co.jp>
2025-07-02 11:20:26 +0900

    :wrench: neovim を自動インストールするようにした

--- script/dotinstaller/lib/install_required_packages.sh
+++ script/dotinstaller/lib/install_required_packages.sh
@@ -6,9 +6,9 @@ source $(dirname "${BASH_SOURCE[0]:-$0}")/utilfuncs.sh
 
 distro=$(whichdistro)
 if [[ $distro == "redhat" ]]; then
-	checkinstall zsh git tmux bc curl wget gawk python3-pip unzip procps jq xsel
+	checkinstall zsh git tmux bc curl wget gawk python3-pip unzip procps jq xsel neovim
 elif [[ $distro == "debian" ]]; then
-	checkinstall zsh git tmux bc curl wget gawk python3-pip unzip procps jq xsel
+	checkinstall zsh git tmux bc curl wget gawk python3-pip unzip procps jq xsel neovim
 else
-	checkinstall zsh git tmux bc curl wget xsel gawk python-pip procps jq xsel
+	checkinstall zsh git tmux bc curl wget xsel gawk python-pip procps jq xsel neovim
 fi
