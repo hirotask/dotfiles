a7b88e235384b2a2a7cbaa98de917bad02c425be
hirotask <hirotaskhashi@gmail.com>
2026-01-02 12:38:54 +0900

    fix: weztermのnightly版をダウンロードするようにした

--- script/dotinstaller/lib/install_terminal_app.sh
+++ script/dotinstaller/lib/install_terminal_app.sh
@@ -2,8 +2,8 @@
 
 set -ue
 
-curl -LO https://github.com/wezterm/wezterm/releases/download/20240203-110809-5046fc22/WezTerm-20240203-110809-5046fc22-Ubuntu20.04.AppImage
-chmod +x WezTerm-20240203-110809-5046fc22-Ubuntu20.04.AppImage
+curl -LO https://github.com/wezterm/wezterm/releases/download/nightly/WezTerm-nightly-Ubuntu20.04.AppImage
+chmod +x WezTerm-nightly-Ubuntu20.04.AppImage
 
 mkdir -p /usr/local/bin/
-mv ./WezTerm-20240203-110809-5046fc22-Ubuntu20.04.AppImage /usr/local/bin/wezterm
+mv WezTerm-nightly-Ubuntu20.04.AppImage /usr/local/bin/wezterm
